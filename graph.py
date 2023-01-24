0c146cb2eb4abb851e7e9bf73cdfb41003d15818
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2023-01-24 08:21:10 +0000

    Jan 24, 2023, 11:51 AM

--- graph.py
+++ graph.py
@@ -32,11 +32,41 @@ G.add_edge(H, M, weight=1) # traget is M
 
 import matplotlib.pyplot as plt
 
-# draw the graph with labels and weights on edges 
-print('G: ', G.edges(data=True))
-nx.draw(G, with_labels=True, font_weight='bold')
-edge_labels = nx.get_edge_attributes(G, 'weight')
-print('edge_labels: ', edge_labels)
-nx.draw_networkx_edge_labels(G, pos=nx.spring_layout(G), edge_labels=edge_labels)
-# show the graph
-plt.show()
+
+
+import networkx as nx
+
+def a_star(graph, start, goal):
+    frontier = [(start, 0)]
+    visited = set()
+    while frontier:
+        current = frontier.pop(0)
+        if current[0] in visited:
+            continue
+        print("Expanding node:", current[0])
+        visited.add(current[0])
+        if current[0] == goal:
+            return current[1]
+        for neighbor in graph[current[0]]:
+            #calculate the cost to move to the neighbor node
+            cost = graph[current[0]][neighbor]['weight']
+            # calculate the heuristic value (estimated cost to reach the goal)
+            heuristic = abs(neighbor - goal)
+            # add the neighbor node to the frontier with the total cost
+            frontier.append((neighbor, current[1] + cost + heuristic))
+            print("Crossing node:", neighbor)
+        frontier = sorted(frontier, key=lambda x: x[1])
+
+# create the graph
+G = nx.Graph()
+G.add_edge(1, 2, weight=1)
+G.add_edge(1, 3, weight=4)
+G.add_edge(2, 4, weight=2)
+G.add_edge(3, 4, weight=1)
+G.add_edge(4, 5, weight=3)
+G.add_edge(4, 6, weight=5)
+G.add_edge(5, 6, weight=1)
+
+# perform A* search
+result = a_star(G, 1, 6)
+print("Shortest path cost:", result)
